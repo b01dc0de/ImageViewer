b90683c0cd67692ce80791022bb68c9795c11bc0
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-06-06 14:40:27 -0500

    ImageViewer -- INIT

---
+++ src/Common.h
@@ -0,0 +1,38 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// C++ Std Lib
+#include <cstdio>
+#include <vector>
+// Win32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+// DX11
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <dxgi.h>
+
+// Types
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using s32 = int;
+
+// Globals
+extern bool bRunning;
+extern HWND hWindow;
+extern UINT WinResX;
+extern UINT WinResY;
+
+#if UNICODE
+    #define APPNAME() (L"ImageViewer")
+#else
+    #define APPNAME() ("ImageViewer")
+#endif
+
+// ImageViewer headers
+#include "Math.h"
+#include "Utils.h"
+
+#endif // COMMON_H
+
